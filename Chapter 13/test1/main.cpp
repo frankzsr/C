c3c64663c93ffe5efe9ca5fb39dadc6de1023ae6
zhaosongru <zhaosongru_frank@163.com>
2019-10-14 16:12:02 +0800

    测试重载

--- Chapter 13/test1/main.cpp
+++ Chapter 13/test1/main.cpp
@@ -28,5 +28,13 @@ int main()
 		cout << ": hasn't a table.\n";
 	}
 
+	player2.Name(10);
+	player2.Name();
+	if (player2.HasTable())
+		cout << ": has a table.\n";
+	else {
+		cout << ": hasn't a table.\n";
+	}
+
 	return 0;
 }
